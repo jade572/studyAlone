74e2e77027af5c529865b9a8dd188d50b04bc44e
LeeJinKyu <jade981220@gmail.com>
2024-08-21 22:00:15 +0900

    Initial Commit

---
+++ studyAlone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  studyAlone
+//
+//  Created by 이진규 on 8/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
